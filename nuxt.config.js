e9a8441068b0ebe50a5286cbd7a076a462e6094b
sctveb <sctveb@gmail.com>
2019-03-27 17:11:39 +0900

    nuxt_bulma_fontawesome_mdi installed

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+export default {
+    css: [
+        'bulma',
+        '@fortawesome/fontawesome-free/css/all.css',
+        'material-design-icons-iconfont/dist/material-design-icons.css'
+    ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+        <span class="icon"><i class="fab fa-accusoft"></i></span>Login
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
